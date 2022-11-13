3bbf13ac17b09922a5ddcb51665e2f696c1a8e66
asif <asifkhan1104@gmail.com>
2022-11-13 19:51:50 +0530

    ui changes

--- app/src/main/java/com/crypto/prices/view/activity/MainActivity2.kt
+++ app/src/main/java/com/crypto/prices/view/activity/MainActivity2.kt
@@ -235,10 +235,10 @@ class MainActivity2 : AppCompatActivity() {
         }
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+    /*override fun onCreateOptionsMenu(menu: Menu): Boolean {
         menuInflater.inflate(R.menu.menu, menu)
         return true
-    }
+    }*/
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when (item.itemId) {
