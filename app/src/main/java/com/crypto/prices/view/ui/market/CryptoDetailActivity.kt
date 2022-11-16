471681144b38b2e034ad5e4c945638a880d7228d
asif <asifkhan1104@gmail.com>
2022-11-16 17:44:48 +0530

    prototype data started flowing in chart

--- app/src/main/java/com/crypto/prices/view/ui/market/CryptoDetailActivity.kt
+++ app/src/main/java/com/crypto/prices/view/ui/market/CryptoDetailActivity.kt
@@ -1,27 +1,36 @@
 package com.crypto.prices.view.ui.market
 
-import android.R
 import android.graphics.Color
-import android.graphics.DashPathEffect
 import android.os.Bundle
 import android.util.Log
 import android.view.MenuItem
 import android.view.View
+import android.widget.SeekBar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.bumptech.glide.Glide
 import com.crypto.prices.CryptoApplication
+import com.crypto.prices.R
 import com.crypto.prices.databinding.ActivityCryptoDetailBinding
 import com.crypto.prices.model.CryptoData
 import com.crypto.prices.utils.NetworkResult
 import com.crypto.prices.view.AppRepositoryImpl
 import com.crypto.prices.view.ViewModelFactory
+import com.github.mikephil.charting.charts.LineChart
+import com.github.mikephil.charting.components.XAxis
+import com.github.mikephil.charting.components.YAxis
+import com.github.mikephil.charting.data.Entry
+import com.github.mikephil.charting.data.LineData
+import com.github.mikephil.charting.data.LineDataSet
+import com.github.mikephil.charting.formatter.IFillFormatter
 
 class CryptoDetailActivity : AppCompatActivity() {
     private var _binding: ActivityCryptoDetailBinding? = null
     private lateinit var mCryptoDetailViewModel: CryptoDetailViewModel
     private val TAG = "CryptoDetailActivity"
+
+    private lateinit var chart:LineChart
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -63,6 +72,94 @@ class CryptoDetailActivity : AppCompatActivity() {
         binding.textViewAtl.text = "$" + data?.atl?.toString()
         binding.textViewAtlPerc.text = data?.atl_change_percentage?.toString()+"%"
 
+        initChart()
+    }
+
+    // intialize chart
+    private fun initChart() {
+        chart = binding.chart
+        chart.setViewPortOffsets(0f, 0f, 0f, 0f)
+        chart.setBackgroundColor(Color.rgb(104, 241, 175))
+
+        // no description text
+        chart.description.isEnabled = false
+
+        // enable touch gestures
+        chart.setTouchEnabled(true)
+
+        // enable scaling and dragging
+        chart.isDragEnabled = true
+        chart.setScaleEnabled(true)
+
+        // if disabled, scaling can be done on x- and y-axis separately
+        chart.setPinchZoom(false)
+
+        chart.setDrawGridBackground(false)
+        chart.maxHighlightDistance = 300f
+
+        val x = chart.xAxis
+        x.isEnabled = false
+
+        val y = chart.axisLeft
+        //y.setTypeface(tfLight);
+        y.setLabelCount(6, false)
+        y.textColor = Color.WHITE
+        y.setPosition(YAxis.YAxisLabelPosition.INSIDE_CHART)
+        y.setDrawGridLines(false)
+        y.axisLineColor = Color.WHITE
+
+        chart.axisRight.isEnabled = false
+
+        chart.legend.isEnabled = false
+
+        chart.animateXY(2000, 2000)
+
+        setData(45, 100f)
+        // don't forget to refresh the drawing
+        chart.invalidate()
+    }
+
+    private fun setData(count: Int, range: Float) {
+        val values = ArrayList<Entry>()
+        for (i in 0 until count) {
+            val `val` = (Math.random() * (range + 1)).toFloat() + 20
+            values.add(Entry(i.toFloat(), `val`))
+        }
+        val set1: LineDataSet
+        if (chart.data != null &&
+            chart.data.dataSetCount > 0
+        ) {
+            set1 = chart.data.getDataSetByIndex(0) as LineDataSet
+            set1.values = values
+            chart.data.notifyDataChanged()
+            chart.notifyDataSetChanged()
+        } else {
+            // create a dataset and give it a type
+            set1 = LineDataSet(values, "DataSet 1")
+            set1.mode = LineDataSet.Mode.CUBIC_BEZIER
+            set1.cubicIntensity = 0.2f
+            set1.setDrawFilled(true)
+            set1.setDrawCircles(false)
+            set1.lineWidth = 1.8f
+            set1.circleRadius = 4f
+            set1.setCircleColor(Color.WHITE)
+            set1.highLightColor = Color.rgb(244, 117, 117)
+            set1.color = Color.WHITE
+            set1.fillColor = Color.WHITE
+            set1.fillAlpha = 100
+            set1.setDrawHorizontalHighlightIndicator(false)
+            set1.fillFormatter =
+                IFillFormatter { dataSet, dataProvider -> chart.axisLeft.axisMinimum }
+
+            // create a data object with the data sets
+            val data = LineData(set1)
+            //data.setValueTypeface(tfLight)
+            data.setValueTextSize(9f)
+            data.setDrawValues(false)
+
+            // set data
+            chart.data = data
+        }
     }
 
     private fun setUpViewModel(data: CryptoData?) {
@@ -85,19 +182,19 @@ class CryptoDetailActivity : AppCompatActivity() {
     private fun onError(s: String) {
         binding.textViewError.text = s
         binding.textViewError.visibility = View.VISIBLE
-        binding.loadingView.visibility = View.GONE
+        //binding.loadingView.visibility = View.GONE
         binding.chart.visibility = View.GONE
     }
 
     private fun onLoading() {
         binding.textViewError.visibility = View.GONE
-        binding.loadingView.visibility = View.VISIBLE
+        //binding.loadingView.visibility = View.VISIBLE
     }
 
     private fun onLoadingFinished() {
         binding.chart.visibility = View.VISIBLE
         binding.textViewError.visibility = View.GONE
-        binding.loadingView.visibility = View.GONE
+        //binding.loadingView.visibility = View.GONE
     }
 
     fun loadData() {
