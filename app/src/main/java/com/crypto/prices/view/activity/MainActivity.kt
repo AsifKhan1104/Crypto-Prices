fe258f044a3e1c2a8b2e9f735e507fd381b8f738
asif <asifkhan1104@gmail.com>
2022-12-05 16:05:27 +0530

    title handling in home

--- app/src/main/java/com/crypto/prices/view/activity/MainActivity.kt
+++ app/src/main/java/com/crypto/prices/view/activity/MainActivity.kt
@@ -110,6 +110,7 @@ class MainActivity : AppCompatActivity() {
 
     // handling show more button from home page
     fun clickExploreMenu() {
+        supportActionBar?.title = getString(R.string.title_explore)
         binding?.bottomNavigationView?.menu.getItem(2).setChecked(true)
         openFragment(newsFragment)
         activeFragment = newsFragment
